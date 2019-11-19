74f3e20b91db5520203caf3244ab2b578fead69e
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2019-11-20 01:14:51 +0300

    stencil config update
    + imported @stencil/sass

---
+++ stencil.config.ts
@@ -0,0 +1,25 @@
+import { Config } from "@stencil/core";
+import { sass } from "@stencil/sass";
+
+export const config: Config = {
+  namespace: "pwc-choices",
+  bundles: [
+    {
+      components: ["pwc-choices"]
+    }
+  ],
+  outputTargets: [
+    {
+      type: "dist",
+      esmLoaderPath: "../loader"
+    },
+    {
+      type: "docs-readme"
+    },
+    {
+      type: "www",
+      serviceWorker: null // disable service workers
+    }
+  ],
+  plugins: [sass()]
+};
